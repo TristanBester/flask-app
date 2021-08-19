9cbf67f8a923a09268806bcb607243c7ee1782ed
TristanBester <tristanbester@gmail.com>
2021-08-20 00:43:01 +0200

    Added additional functionality and unit tests.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Email, Length
 from app.models import User
 
 
@@ -31,3 +31,24 @@ class RegistrationForm(FlaskForm):
 
         if user is not None:
             raise ValidationError("Email address is already taken.")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+
+    def __init__(self, username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+
+            if user is not None:
+                raise ValidationError("That username is already taken.")
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
 from app.models import User
 from app import db
 
@@ -64,3 +65,85 @@ def register():
         flash("Registration complete.")
         return redirect(url_for("index"))
     return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"},
+    ]
+    form = EmptyForm()
+    return render_template("user.html", user=user, posts=posts, form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm(username=current_user.username)
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit profile", form=form)
+
+
+@app.route("/follow/<username>", methods=["POST"])
+@login_required
+def follow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            flash(f"User {username} not found.")
+            return redirect(url_for("index"))
+        if user == current_user:
+            flash("You cannot follow yourself.")
+            return redirect(url_for("user", username=username))
+
+        current_user.follow(user)
+        db.session.commit()
+        flash(f"Your are now following {username}.")
+        return redirect(url_for("user", username=username))
+    else:
+        return redirect(url_for("index"))
+
+
+@app.route("/unfollow/<username>", methods=["POST"])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            flash(f"User {username} not found.")
+            return redirect(url_for("index"))
+        if user == current_user:
+            flash("You cannot unfollow yourself.")
+            return redirect(url_for("user", username=username))
+
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f"You are no longer following {username}")
+        return redirect(url_for("user", username=username))
+    else:
+        return redirect(url_for("index"))
