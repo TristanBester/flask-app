2f7f1daf02932671b3fbe0fd707bc97fb54320a0
TristanBester <tristanbester@gmail.com>
2021-08-22 02:07:47 +0200

    Added additional language support.

--- app/email.py
+++ app/email.py
@@ -1,10 +1,11 @@
-from flask_mail import Message
-from flask import render_template
 from threading import Thread
+from flask import render_template
+from flask_mail import Message
+from flask_babel import _
 from app import app, mail
 
 
-def send_email_async(app, msg):
+def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
@@ -13,13 +14,13 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_email_async, args=(app, msg)).start()
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
-        "Flask-app reset your password",
+        _("[Microblog] Reset Your Password"),
         sender=app.config["ADMINS"][0],
         recipients=[user.email],
         text_body=render_template("email/reset_password.txt", user=user, token=token),
--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from sqlalchemy.orm import defaultload
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
-from app import db, login, app
-import time
+from time import time
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import app, db, login
 
 
 followers = db.Table(
@@ -19,11 +18,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128), index=True, unique=True)
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.Date, default=datetime.utcnow())
-
+    password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
         "User",
         secondary=followers,
@@ -33,6 +31,9 @@ class User(UserMixin, db.Model):
         lazy="dynamic",
     )
 
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -41,7 +42,9 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
-        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
+            digest, size
+        )
 
     def follow(self, user):
         if not self.is_following(user):
@@ -58,14 +61,12 @@ class User(UserMixin, db.Model):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)
         ).filter(followers.c.follower_id == self.id)
-
         own = Post.query.filter_by(user_id=self.id)
-
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def get_reset_password_token(self, expires_in=6000):
+    def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
-            {"reset_password": self.id, "exp": time.time() + expires_in},
+            {"reset_password": self.id, "exp": time() + expires_in},
             app.config["SECRET_KEY"],
             algorithm="HS256",
         )
@@ -80,8 +81,10 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
-    def __repr__(self):
-        return f"<User {self.username}>"
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -91,9 +94,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __repr__(self):
-        return f"<Post {self.body}>"
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+        return "<Post {}>".format(self.body)
